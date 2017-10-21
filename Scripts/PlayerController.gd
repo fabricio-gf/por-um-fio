19b6970b442b85083f66704233c2de97f1d2bd52
Fabricio Guedes <fabricio-gf@hotmail.com>
2017-10-21 15:19:18 -0200

    Movement (no jump) and collision

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,17 +1,36 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
+const GRAVITY = 200.0
+const MOVESPEED = 100.0
+var vel = Vector2()
 export var p_number = 0
 
 func _ready():
-	set_process_input(true)
+	set_fixed_process(true)
+	# set_process_input(true)
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
+	
+func _fixed_process(delta):
+	vel.y += delta * GRAVITY
 
-func _input(event):
-	if event.is_action_pressed(str("left_joy_p",p_number)): print("left ", p_number)
-	if event.is_action_pressed(str("right_joy_p",p_number)): print("right ", p_number)
+	if Input.is_action_pressed(str("left_joy_p",p_number)): 
+		vel.x = -MOVESPEED
+	elif Input.is_action_pressed(str("right_joy_p",p_number)): 
+		vel.x = MOVESPEED
+	else: 
+		vel.x = 0
+	
+	var motion = vel * delta
+	move(motion)
+	
+	if (is_colliding()):
+        var n = get_collision_normal()
+        motion = n.slide(motion)
+        vel = n.slide(vel)
+        move(motion)
