3702fe35ad92838fb74319e49af88652f293210d
gael-sng <gael.sng@gmail.com>
2017-10-22 17:52:08 -0200

    botoes funcionando

--- Scripts/Botão.gd
+++ Scripts/Botão.gd
@@ -1,13 +1,27 @@
-extends CollisionShape2D
+extends Area2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
+var pressed = 0
+
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	if .collide(b_xform, a, a_xform):
-		print("OMG Collision!")
-	
+	set_process(true)
 	pass
 
+func _process(delta):
+	if pressed > 0:
+		set_scale(Vector2(1,0.5))
+	else:
+		set_scale(Vector2(1,1))
+		
+	pass
+
+func _on_BotaoTrigger_area_enter( area ):
+	pressed+=1
+	pass # replace with function body
+
+
+func _on_BotaoTrigger_area_exit( area ):
+	pressed-=1
+	pass # replace with function body
